fc3ff2a51a364a4d482ce4e592ceea92da7b3291
João Paulo <joaopaulosg17@gmail.com>
2025-05-01 14:48:22 -0300

    Adicionada classe para conexão com o db

--- src/main/java/projeto/faculdade/App.java
+++ src/main/java/projeto/faculdade/App.java
@@ -1,8 +1,12 @@
 package projeto.faculdade;
 
+import java.sql.Connection;
+
+import projeto.faculdade.util.DbConnection;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws Exception{
+        Connection con = DbConnection.getConnection();
+        System.out.println(con);
     }
 }
-w
